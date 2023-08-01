dda37b725d19f175448779bebf766336943dccb2
kiyo650 <135185159+kiyo650@users.noreply.github.com>
2023-08-01 22:12:02 +0900

    Update gators.js

--- gators.js
+++ gators.js
@@ -1,29 +1,24 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
 
-// function gator(){
-//   let element = document.getElementById("number")
-//   let result = ""
-//   for (let i = 0; i < element.value; i++){
-//     const happen = document.createElement('p');
-//     happen.textContent = "🐊";
-//     document.body.append(happen);
-//   }
-//   return result
-// }
-
-function happenGators(){
+function goGators() {
     let numberGators = document.getElementById("number");
-    let result ="";
-    for (let i = 0; i < numberGators.value; i++){
-    const newDiv = document.createElement("div");
-    const gator = document.createTextNode("🐊");
-    newDiv.appendChild(gator);
-    const currentDiv = document.getElementById("gator");
-    document.body.insertBefore(newDiv, currentDiv)
+    let result = "";
+    for (let i = 0; i < numberGators.value; i++) {
+        const newDiv = document.createElement("div");
+        const gator = document.createTextNode("🐊");
+        newDiv.appendChild(gator);
+        const currentDiv = document.getElementById("gator");
+        document.body.insertBefore(newDiv, currentDiv)
     }
     return result
 }
+const go = document.getElementById("go");
+go.addEventListener("click", goGators)
 
-const target = document.getElementById("target");
-target.addEventListener("click", happenGators)
+function homeGators() {
+    let result = document.newDiv
+    return result.innerText = ""
+}
+const home = document.getElementById("home");
+home.addEventListener("click", homeGators)
